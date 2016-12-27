59bd88a484e1f754ab1dcb58d462771076a1194a
hurelhuyag <hurelhuyag@sodonsolution.com>
2016-12-27 20:48:44 +0800

    call back

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -7,30 +7,34 @@ import android.content.Intent;
 
 public class Hello extends CordovaPlugin {
 
+    private CallbackContext callbackContext;
+
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        /*if (action.equals("greet")) {
+        if (action.equals("shareToFacebook")) {
 
-            String name = data.getString(0);
-            String message = "Hello, " + name;
-            callbackContext.success(message);
-
+            this.callbackContext = callbackContext;
+            Intent intent1 = new Intent();
+            intent1.setClassName("com.facebook.katana", "com.facebook.composer.shareintent.ImplicitShareIntentHandlerAlphabeticalAlias");
+            intent1.setAction("android.intent.action.SEND");
+            intent1.setType("text/plain");
+            intent1.putExtra("android.intent.extra.TEXT", data.getString(0));
+            cordova.setActivityResultCallback (this);
+            cordova.getActivity().startActivityForResult(intent1, 1051);
             return true;
 
         } else {
             
             return false;
 
-        }*/
+        }
+    }
 
-        Intent intent1 = new Intent();
-        intent1.setClassName("com.facebook.katana", "com.facebook.composer.shareintent.ImplicitShareIntentHandlerAlphabeticalAlias");
-        intent1.setAction("android.intent.action.SEND");
-        intent1.setType("text/plain");
-        intent1.putExtra("android.intent.extra.TEXT", data.getString(0));
-        cordova.getActivity().startActivityForResult(intent1, 1051);
-        callbackContext.success("dialog returned");
-        return true;
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == 1051 && callbackContext != null){
+            callbackContext.success("dialog returned");
+        }
     }
 }
