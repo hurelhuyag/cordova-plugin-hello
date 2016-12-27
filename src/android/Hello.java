c0850a24a31103c853a41e1d36d41319008fbf01
hurelhuyag <hurelhuyag@sodonsolution.com>
2016-12-27 20:39:27 +0800

    start activity test

--- src/android/Hello.java
+++ src/android/Hello.java
@@ -3,13 +3,14 @@ package com.example.plugin;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import android.content.Intent;
 
 public class Hello extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
 
-        if (action.equals("greet")) {
+        /*if (action.equals("greet")) {
 
             String name = data.getString(0);
             String message = "Hello, " + name;
@@ -21,6 +22,15 @@ public class Hello extends CordovaPlugin {
             
             return false;
 
-        }
+        }*/
+
+        Intent intent1 = new Intent();
+        intent1.setClassName("com.facebook.katana", "com.facebook.composer.shareintent.ImplicitShareIntentHandlerAlphabeticalAlias");
+        intent1.setAction("android.intent.action.SEND");
+        intent1.setType("text/plain");
+        intent1.putExtra("android.intent.extra.TEXT", data.getString(0));
+        cordova.getActivity().startActivityForResult(intent1, 1051);
+        callbackContext.success("dialog returned");
+        return true;
     }
 }
